06eebe109c5664df850029e6ace81779cac1a64d
Joel Scoble <joel.scoble@outlook.com>
2016-06-20 20:50:26 -0500

    initial commit

---
+++ csv2htmltable.go
@@ -0,0 +1,29 @@
+package csv2htmltable
+
+import (
+	"html/template"
+	"io"
+)
+
+var tableTpl = `
+<table class="{{.Class}}">
+{{range $row := .CSV}}    <tr>
+        {{range $row}}<td>{{.}}</td>{{end}}
+    </tr>
+{{end}}</table>
+`
+
+type HTMLTable struct {
+	Class string
+	ID    string
+	CSV   [][]string
+	tpl   *template.Template
+}
+
+func New(n string) *HTMLTable {
+	return &HTMLTable{tpl: template.Must(template.New(n).Parse(tableTpl))}
+}
+
+func (h *HTMLTable) Write(w io.Writer) error {
+	return h.tpl.Execute(w, h)
+}
---
+++ csv2htmltable_test.go
@@ -0,0 +1,85 @@
+package csv2htmltable
+
+import (
+	"bytes"
+	"testing"
+)
+
+func TestWrite(t *testing.T) {
+	tests := []struct {
+		Class    string
+		CSV      [][]string
+		Expected string
+	}{
+		{
+			Class: "",
+			CSV: [][]string{
+				[]string{"a", "b", "c"},
+				[]string{"1", "2", "3"},
+			},
+			Expected: `
+<table class="">
+    <tr>
+        <td>a</td><td>b</td><td>c</td>
+    </tr>
+    <tr>
+        <td>1</td><td>2</td><td>3</td>
+    </tr>
+</table>
+`,
+		},
+		{
+			Class: "test",
+			CSV: [][]string{
+				[]string{"a", "b", "c"},
+				[]string{"1", "2", "3"},
+			},
+			Expected: `
+<table class="test">
+    <tr>
+        <td>a</td><td>b</td><td>c</td>
+    </tr>
+    <tr>
+        <td>1</td><td>2</td><td>3</td>
+    </tr>
+</table>
+`,
+		},
+		{
+			Class: "people",
+			CSV: [][]string{
+				[]string{"Greeting", "Title", "Name"},
+				[]string{"Hello", "Mr.", "Bob"},
+				[]string{"Bonjour", "M.", "Genvieve"},
+			},
+			Expected: `
+<table class="people">
+    <tr>
+        <td>Greeting</td><td>Title</td><td>Name</td>
+    </tr>
+    <tr>
+        <td>Hello</td><td>Mr.</td><td>Bob</td>
+    </tr>
+    <tr>
+        <td>Bonjour</td><td>M.</td><td>Genvieve</td>
+    </tr>
+</table>
+`,
+		},
+	}
+	var buf bytes.Buffer
+	h := New("test")
+	for i, test := range tests {
+		buf.Reset()
+		h.Class = test.Class
+		h.CSV = test.CSV
+		err := h.Write(&buf)
+		if err != nil {
+			t.Errorf("%d: got %q: want nil", i, err)
+			continue
+		}
+		if buf.String() != test.Expected {
+			t.Errorf("%d got %s; want %s", i, buf.String(), test.Expected)
+		}
+	}
+}
