6ce201598f2a6a0634d5d900595f1dcdf900de2d
Joel Scoble <joel.scoble@outlook.com>
2016-06-28 16:41:16 -0500

    add flags for the new HTMLTable config options

--- cmd/csv2htmltable/main.go
+++ cmd/csv2htmltable/main.go
@@ -10,20 +10,36 @@ import (
 )
 
 var (
-	input     string
-	output    string
-	class     string
-	rowHeader bool
-	id        string
-	caption   string
-	footer    string
+	input  string
+	output string
+
+	section     bool
+	headingText string
+	headingTag  int
+
+	class        string
+	id           string
+	caption      string
+	tableHeader  bool
+	headerRowNum int
+	rowHeader    bool
+	footer       string
 )
 
 func init() {
+	// c d f h i n o p r s x
 	flag.StringVar(&input, "input", "stdin", "the path to the input file; if not specified stdin is used")
 	flag.StringVar(&input, "i", "stdin", "the short flag for -input")
 	flag.StringVar(&output, "output", "stdout", "output destination")
 	flag.StringVar(&output, "o", "stdout", "output destination (short)")
+
+	flag.BoolVar(&section, "section", false, "create the table in its own section")
+	flag.BoolVar(&section, "s", false, "create the table in its own section")
+	flag.StringVar(&headingText, "headingtext", "", "text for the heading element")
+	flag.StringVar(&headingText, "x", "", "text for the heading element")
+	flag.IntVar(&headingTag, "headingtag", 4, "int representing the heading tag size: valid values are 1-6")
+	flag.IntVar(&headingTag, "t", 4, "int representing the heading tag size: valid values are 1-6")
+
 	flag.StringVar(&class, "class", "", "the table's class")
 	flag.StringVar(&class, "c", "", "the table's class")
 	flag.StringVar(&id, "id", "", "the table's id")
@@ -32,6 +48,10 @@ func init() {
 	flag.StringVar(&caption, "p", "", "table caption")
 	flag.StringVar(&footer, "footer", "", "table footer")
 	flag.StringVar(&footer, "f", "", "table footer")
+	flag.BoolVar(&tableHeader, "tableheader", true, "include table heading in the output")
+	flag.BoolVar(&tableHeader, "h", true, "include table heading in the output")
+	flag.IntVar(&headerRowNum, "headerrownum", 1, "number of header rows in the csv")
+	flag.IntVar(&headerRowNum, "n", 1, "number of header rows in the csv")
 	flag.BoolVar(&rowHeader, "rowheader", false, "make the first column of each row a header")
 	flag.BoolVar(&rowHeader, "r", false, "make the first column of each row a header")
 }
@@ -82,7 +102,13 @@ func realMain() int {
 		htable.Footer = footer
 	}
 	htable.RowHeader = rowHeader
-
+	htable.Section = section
+	if headingText != "" {
+		htable.HeadingText = headingText
+	}
+	htable.HeadingTag = headingTag
+	htable.TableHeader = tableHeader
+	htable.HeaderRowNum = headerRowNum
 	r := csv.NewReader(in)
 	htable.CSV, err = r.ReadAll()
 	if err != nil {
